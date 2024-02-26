38fc22d1cddec3c3f1f0f878e188b49b487d911f
JavaVioliNina <niag024@ukr.net>
2024-02-26 22:44:22 +0200

    Converter of the temperature

---
+++ Lesson4/src/main/java/Lesson4_2.java
@@ -0,0 +1,3 @@
+public class Lesson4_2 {
+
+}
---
+++ Lesson4_10/src/main/java/Converter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+public class Converter {
+    public static void main(String[] args) {
+
+//    // (F = C * 1.8 + 32)
+        System.out.println("Temperature C:" + 50);
+        System.out.println("Temperature F =" + (50 * 1.8 + 32));
+
+        int c = new Scanner(System.in).nextInt;
+        double formula = c * 1.8 + 32;
+
+        System.out.println("Please, write down your temperature:");
+        System.out.println();
+    }
+}
