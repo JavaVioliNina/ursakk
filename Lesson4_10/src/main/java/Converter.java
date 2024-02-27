385579e461b2e25afebbe4833f8f310301c9e84c
JavaVioliNina <niag024@ukr.net>
2024-02-27 21:32:47 +0200

    Converter C-F

--- Lesson4_10/src/main/java/Converter.java
+++ Lesson4_10/src/main/java/Converter.java
@@ -1,4 +1,4 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Converter {
     public static void main(String[] args) {
@@ -7,7 +7,7 @@ public class Converter {
         System.out.println("Temperature C:" + 50);
         System.out.println("Temperature F =" + (50 * 1.8 + 32));
 
-        int c = new Scanner(System.in).nextInt;
+        int c = new Scanner(System.in).nextInt();
         double formula = c * 1.8 + 32;
 
         System.out.println("Please, write down your temperature:");
