898d5a7bcb9946d950fd293cdbee84ed9314d06b
JavaVioliNina <niag024@ukr.net>
2024-02-28 11:22:29 +0200

    UrsaWeight

---
+++ Weight/src/main/java/UrsaWeight.java
@@ -0,0 +1,10 @@
+public class UrsaWeight {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the UrsaWeight team!");
+        int UrsaKg = 24;
+        int BarsikKg = 4;
+        int UrsakKg = 153;
+        System.out.println("Middle weight of members UtsaWeight team:");
+        System.out.println((UrsaKg + BarsikKg + UrsakKg) / 3);
+    }
+}
